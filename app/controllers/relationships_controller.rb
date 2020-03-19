060ff494f7a45fdb392ce2629bba86e673e40f7c
Rails udemy course <Railsudemycourse@gmail.com>
2020-03-19 08:57:15 +0530

    Build logic in show.html.erb, change in other file

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,19 +1,19 @@
 class RelationshipsController < ApplicationController
   before_action :authenticate_user!
-
+  before_action :identify_user
+  
   def create
     current_user.follow(@user)
     redirect_to :back
   end
 
   def destroy
-    curent_user.unfollow(@user)
+    current_user.unfollow(@user)
     redirect_to :back
   end
-  
-  private
 
-  def identify_user
-    @user = User.find_by(params[:id])
-  end
+  private
+    def identify_user
+    @user = User.find_by(params[:user_id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_for :users, :controllers => { registrations: 'registrations', users: 'users' }
+  
+
 	
   resources :tweets 
   root 'tweets#index'
